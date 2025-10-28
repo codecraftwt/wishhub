57aa54653c47b5d01d07cae6f32d46ef9e88ca90
codecraftwt <codecraftwt@gmail.com>
2025-10-28 15:12:42 +0530

    Revert "Migrate from SQLite to PostgreSQL for data persistence"
    This reverts commit 9e2ca73f900c9f245a487cbb6b6b1b7795813228.

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="@remix-run/node" />
-
-interface ImportMetaEnv {
-  readonly DATABASE_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
